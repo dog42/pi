eb9689756227faa6e868f0c333ea1d6cbd356905
D0gi <s122106@hft-leipzig.de>
2016-09-20 18:23:35 +0200

    update go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo TROLOLOLOLOOOOOOO
 
+# content of /etc/rc-local:
 # wget https://github.com/D0gi/pi/archive/master.zip -O /home/pi/.master.zip
 # unzip /home/pi/.master.zip -d /home/pi/.piconf
-# rm .master.zip
+# rm /home/pi/.master.zip
 # ./home/pi/.piconf/pi-master/go.sh
