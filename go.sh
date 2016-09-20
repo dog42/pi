32f955ad56f5f24df264b8e1b2d28394bb05cc48
D0gi <s122106@hft-leipzig.de>
2016-09-20 18:41:25 +0200

    test zsh install

--- go.sh
+++ go.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo TROLOLOLOLOOOOOOO
 
+sudo apt install -y zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -r /home/pi/.piconf/pi-master/.oh-my-zsh home/pi/
+echo "going to REBOOT"
+sleep 10
+sudo reboot
+
 # content of /etc/rc-local:
 # wget https://github.com/D0gi/pi/archive/master.zip -O /home/pi/.master.zip
 # unzip /home/pi/.master.zip -d /home/pi/.piconf
