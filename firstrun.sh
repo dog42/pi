86f2390bdace98d9244f516829d47c06176d91ea
D0gi <s122106@hft-leipzig.de>
2016-09-20 19:39:30 +0200

    change setting for keyboard

--- firstrun.sh
+++ firstrun.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo 'XKBMODEL="pc105"\nXKBLAYOUT="de"\nXKBVARIANT=""\nXKBOPTIONS=""' > /etc/default/keyboard
+echo 'XKBMODEL="pc105"' > /etc/default/keyboard
+echo 'XKBLAYOUT="de"' >> /etc/default/keyboard
+echo 'XKBVARIANT=""' >> /etc/default/keyboard
+echo 'XKBOPTIONS=""' >> /etc/default/keyboard
 echo "keyboard set to de"
 
+sudo apt-get update
 sudo apt install -y git zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
