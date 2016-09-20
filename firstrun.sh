b47e2ae2d919108f8d178a7ad955b6e3bc26e839
D0gi <s122106@hft-leipzig.de>
2016-09-20 20:10:02 +0200

    ...

--- firstrun.sh
+++ firstrun.sh
@@ -4,10 +4,11 @@ echo 'XKBMODEL="pc105"' > /etc/default/keyboard
 echo 'XKBLAYOUT="de"' >> /etc/default/keyboard
 echo 'XKBVARIANT=""' >> /etc/default/keyboard
 echo 'XKBOPTIONS=""' >> /etc/default/keyboard
+ech0 'LANG=de_DE.UTF-8' > /etc/default/locale
 echo "keyboard set to de"
 
 sudo apt-get update
 sudo apt install -y git zsh
-sudo -u pi chsh -s $(which zsh)
+chsh -s $(which zsh) pi
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -r /home/pi/.piconf/pi-master/.oh-my-zsh home/pi/
+cp -r /home/pi/.piconf/pi-master/.oh-my-zsh /home/pi/.piconf/pi-master/.zshrc home/pi/
