4d9b19c1b881ce3632ba88572b0592b2d73830fe
D0gi <s122106@hft-leipzig.de>
2016-09-20 16:48:06 +0200

    make go.sh executable

--- go.sh
+++ go.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo TROLOLOLOLOOOOOOO
+
+# wget https://github.com/D0gi/pi/archive/master.zip -O /home/pi/.master.zip
+# unzip /home/pi/.master.zip -d /home/pi/.piconf
+# rm .master.zip
+# ./home/pi/.piconf/pi-master/go.sh
