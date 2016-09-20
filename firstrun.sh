cc76d6d896db656f8c5a03304202f444f0a6a429
D0gi <s122106@hft-leipzig.de>
2016-09-20 19:18:54 +0200

    change keyboard

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'XKBMODEL="pc105"\nXKBLAYOUT="de"\nXKBVARIANT=""\nXKBOPTIONS=""' > /etc/default/keyboard
+echo "keyboard set to de"
+
 sudo apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
