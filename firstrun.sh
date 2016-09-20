7735adab546a15df19c6b3cb08f584d861fcf5f8
D0gi <s122106@hft-leipzig.de>
2016-09-20 20:25:29 +0200

    ...

--- firstrun.sh
+++ firstrun.sh
@@ -9,6 +9,6 @@ echo "keyboard set to de"
 
 sudo apt-get update
 sudo apt install -y git zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo -u pi sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -r /home/pi/.piconf/pi-master/.oh-my-zsh /home/pi/.piconf/pi-master/.zshrc home/pi/
 chsh -s $(which zsh) pi
