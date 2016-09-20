2c9dc22178b8f4d8c61b06800716e0843673441a
D0gi <s122106@hft-leipzig.de>
2016-09-20 19:33:51 +0200

    add git ro firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -3,7 +3,7 @@
 echo 'XKBMODEL="pc105"\nXKBLAYOUT="de"\nXKBVARIANT=""\nXKBOPTIONS=""' > /etc/default/keyboard
 echo "keyboard set to de"
 
-sudo apt install -y zsh
+sudo apt install -y git zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -r /home/pi/.piconf/pi-master/.oh-my-zsh home/pi/
