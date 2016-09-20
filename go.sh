83d81d82bf329b5103f3591f0486916ee040af48
D0gi <s122106@hft-leipzig.de>
2016-09-20 18:56:29 +0200

    rm bug

--- go.sh
+++ go.sh
@@ -5,9 +5,7 @@ sudo apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -r /home/pi/.piconf/pi-master/.oh-my-zsh home/pi/
-echo "going to REBOOT"
-sleep 10
-sudo reboot
+
 
 # content of /etc/rc-local:
 # wget https://github.com/D0gi/pi/archive/master.zip -O /home/pi/.master.zip
