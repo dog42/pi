6fa879fcb02cc10ed52f549c50dc3a4b7b7eeefc
D0gi <s122106@hft-leipzig.de>
2016-09-26 15:30:20 +0200

    ...

--- firstrun.sh
+++ firstrun.sh
@@ -8,11 +8,11 @@ echo 'LANG=de_DE.UTF-8' > /etc/default/locale
 echo "keyboard set to de"
 
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt install -y git zsh
+sudo apt-get -y upgrade
+sudo apt install -y git zsh htop
 sudo -u pi sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -r /home/pi/.piconf/pi-master/.oh-my-zsh /home/pi/.piconf/pi-master/.zshrc home/pi/
 chsh -s $(which zsh) pi
-echo going to reboot
+echo ">>>going to reboot"
 sleep 6
 reboot
--- go.sh
+++ go.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo LETS START
-whoamicd 
+echo ">>>LETS START"
+whoami
 
 if [ ! -f /home/pi/.state  ]; then
         echo "1" > /home/pi/.state
-        echo "das erste mal"
+        echo ">>>das erste mal"
         ./home/pi/.piconf/pi-master/firstrun.sh
 else
-        echo "schon alt"
+        echo ">>>schon alt"
 fi
 
 # content of /etc/rc-local:
@@ -15,5 +15,6 @@ fi
 # unzip /home/pi/.master.zip -d /home/pi/.piconf
 # rm /home/pi/.master.zip
 # ./home/pi/.piconf/pi-master/go.sh
-echo "cleanup"
-sudo rm -rf home/pi/.master.zip /home/pi/.piconf
+echo ">>>cleanup"
+sudo rm -rf /home/pi/.master.zip 
+sudo rm -rf /home/pi/.piconf
