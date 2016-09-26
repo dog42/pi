53cac8d25c28c0568699922e72244ed5b5b57ecd
D0gi <s122106@hft-leipzig.de>
2016-09-26 11:35:54 +0200

    add .gitignore

--- firstrun.sh
+++ firstrun.sh
@@ -8,6 +8,7 @@ echo 'LANG=de_DE.UTF-8' > /etc/default/locale
 echo "keyboard set to de"
 
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt install -y git zsh
 sudo -u pi sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -r /home/pi/.piconf/pi-master/.oh-my-zsh /home/pi/.piconf/pi-master/.zshrc home/pi/
