f57b7896d1656169e0c2d25e00415b5a9d0f2997
D0gi <s122106@hft-leipzig.de>
2016-09-26 19:20:06 +0200

    add function to set hostname

--- go.sh
+++ go.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-clean() {
+function clean() {
 	echo ">>>cleanup"
 	sudo rm -rf /home/pi/.master.zip 
 	sudo rm -rf /home/pi/.piconf
 }
 
+function sethostname(){
+	ip=$(ip -4 a s eth0)
+	regex1='((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])'
+	if [[ $ip =~ $regex1 ]]
+		then
+		echo pi${BASH_REMATCH[3]} >> /etc/hostname
+		echo ">>>HOSTNAME SET TO: pi${BASH_REMATCH[3]}"
+	fi
+}
+
+
 echo ">>>LETS START"
 whoami
+sethostname
+
 
 if [ ! -f /home/pi/.state  ]; then
 	echo "1" > /home/pi/.state
