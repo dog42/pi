ff734a795a9fe3e12c3ebb56925bfdd96be41d83
D0gi <s122106@hft-leipzig.de>
2016-09-26 19:23:47 +0200

    ...

--- go.sh
+++ go.sh
@@ -11,7 +11,7 @@ function sethostname(){
 	regex1='((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])'
 	if [[ $ip =~ $regex1 ]]
 		then
-		echo pi${BASH_REMATCH[3]} >> /etc/hostname
+		echo pi${BASH_REMATCH[3]} > /etc/hostname
 		echo ">>>HOSTNAME SET TO: pi${BASH_REMATCH[3]}"
 	fi
 }
